446e549b837c52bd7d7fe24ba8226ddef7ef29f4
Michael Reeves <mreeves68@yahoo.com>
2015-07-04 12:43:36 -0600

    Shows individual tasks

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -29,4 +29,12 @@ class TaskManager
     raw_tasks.map { |data| Task.new(data) }
   end
 
+  def self.raw_task(id)
+    raw_tasks.find { |task| task['id'] == id }
+  end
+
+  def self.find(id)
+    Task.new(raw_task(id))
+  end
+
 end
